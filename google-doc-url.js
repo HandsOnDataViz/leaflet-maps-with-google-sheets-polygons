86882e4d3ce260a1b71ad4e2975d1524191552e0
DJanga2002 <dmpereira2002@yahoo.com>
2017-04-06 13:32:02 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,1 @@
-var googleDocURL = https://docs.google.com/spreadsheets/d/1wgW15P1r4aMkx52CgVPekkcC8sV_9VO0IDoUuiMW9vw/edit#gid=0
-
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wgW15P1r4aMkx52CgVPekkcC8sV_9VO0IDoUuiMW9vw/edit#gid=0';
